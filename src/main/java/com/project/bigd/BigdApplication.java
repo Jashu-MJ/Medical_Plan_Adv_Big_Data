11deb7273bcbc54bad7b7c6072ecb8fc9388d848
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-09-28 12:14:52 -0400

    Demo-1: init setup

---
+++ src/main/java/com/project/bigd/BigdApplication.java
@@ -0,0 +1,13 @@
+package com.project.bigd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bigd/BigdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bigd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
